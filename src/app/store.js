771df94fdea33cfb231ad407ade43d0c850fe2c3
Vanshaj Pahwa <vanshajpahwa07@gmail.com>
2024-01-12 16:50:27 +0530

    Changing folder structure

--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import workoutReducer from '../features/WorkoutManagement/workoutSlice';
-
-export const store = configureStore({
-  reducer: {
-    workout: workoutReducer,
-  },
-});
