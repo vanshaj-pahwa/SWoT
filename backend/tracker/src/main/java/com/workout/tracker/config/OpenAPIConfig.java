173a233dd32a812b6172f29649c230fc9b1c20ce
Surabhi0910 <surabhi.roy34@gmail.com>
2024-01-18 20:24:09 +0530

    added authorization token section for swagger

--- backend/tracker/src/main/java/com/workout/tracker/config/OpenAPIConfig.java
+++ backend/tracker/src/main/java/com/workout/tracker/config/OpenAPIConfig.java
@@ -1,10 +1,14 @@
 package com.workout.tracker.config;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@SecurityScheme(name = "bearerAuth", type = SecuritySchemeType.HTTP, bearerFormat = "JWT", scheme = "bearer")
 @Configuration
 public class OpenAPIConfig {
 
@@ -13,6 +17,7 @@ public class OpenAPIConfig {
         return new OpenAPI()
                 .info(new Info().title("Workout Tracker Api")
                         .description("Workout service Apis")
-                        .version("1.0"));
+                        .version("1.0"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
     }
 }
