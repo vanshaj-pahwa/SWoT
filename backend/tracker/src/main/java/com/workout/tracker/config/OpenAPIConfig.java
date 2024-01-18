a1b813d4b63a1f8ad609a75aa828475ec2a50706
Surabhi0910 <surabhi.roy34@gmail.com>
2024-01-18 20:06:45 +0530

    added swagger-ui

---
+++ backend/tracker/src/main/java/com/workout/tracker/config/OpenAPIConfig.java
@@ -0,0 +1,18 @@
+package com.workout.tracker.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI registrationOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Workout Tracker Api")
+                        .description("Workout service Apis")
+                        .version("1.0"));
+    }
+}
